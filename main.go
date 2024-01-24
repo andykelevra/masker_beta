e6eed1ed3679828a396747b85204d80b7882ebbb
Andy <andykelevra92@gmail.com>
2024-01-24 16:15:13 +0300

    w/out testing

---
+++ main.go
@@ -0,0 +1,60 @@
+//	func spamMasker(s, spam string) string {
+//		index := strings.Index(s, spam)
+//		if index == -1 {
+//			return s
+//		}
+//
+//		bytes := []byte(s)
+//		startIndex := index + 7
+//
+//		for j := startIndex; j < len(bytes); j++ {
+//			if bytes[j] == ' ' {
+//				break
+//			} else {
+//				bytes[j] = '*'
+//			}
+//		}
+//		return string(bytes)
+//	}
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func spamMasker(s, spam string) string {
+	bytes := []byte(s)
+	subBytes := []byte(spam)
+
+	for i := 0; i < len(s); i++ {
+		match := true
+		for j := 0; j < len(spam); j++ {
+			if i+j >= len(s) || bytes[i+j] != subBytes[j] {
+				match = false
+			}
+		}
+		if match {
+			for j := i + len(spam); j < len(s); j++ {
+				if bytes[j] != ' ' {
+					bytes[j] = '*'
+				} else {
+					break
+				}
+
+			}
+			i = i + len(spam)
+		}
+	}
+	return string(bytes)
+}
+
+func main() {
+	substring := "http://"
+	myscanner := bufio.NewScanner(os.Stdin)
+	myscanner.Scan()
+	s := myscanner.Text()
+	maskedString := spamMasker(s, substring)
+	fmt.Println(maskedString)
+}
