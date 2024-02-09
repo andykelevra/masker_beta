91525995d158a12377cf9a3a9556d5adcee7f92d
Andy <andykelevra92@gmail.com>
2024-02-09 13:10:43 +0300

    new test

--- main_test.go
+++ main_test.go
@@ -4,36 +4,28 @@ import "testing"
 
 func TestSpamMasker(t *testing.T) {
 
-	t.Run("first", func(t *testing.T) {
-		s := "Here's my spammy page: http://hehefouls.netHAHAHA see you."
-		expected := "Here's my spammy page: http://******************* see you."
-		substring := "http://"
-		result := SpamMasker(s, substring)
+	testCases := []string{"Here's my spammy page: http://hehefouls.netHAHAHA see you.",
+		"Here's my spammy page: http://hehefouls.net HAHAHA see you.",
+		"Here's my spammy page: hTTp://youth-elixir.com",
+		"Here's my spammy page: hTTp://",
+		"http://elixir.com Here's my spammy page",
+	}
+	expectedCases := []string{"Here's my spammy page: http://******************* see you.",
+		"Here's my spammy page: http://************* HAHAHA see you.",
+		"Here's my spammy page: hTTp://youth-elixir.com",
+		"Here's my spammy page: hTTp://",
+		"http://********** Here's my spammy page",
+	}
 
-		if result != expected {
-			t.Errorf("incorrect result, expect %s, got %s", expected, result)
-		}
-	})
+	substring := "http://"
 
-	t.Run("second", func(t *testing.T) {
-		s := "Here's my spammy page: http://hehefouls.net HAHAHA see you."
-		expected := "Here's my spammy page: http://************* HAHAHA see you."
-		substring := "http://"
-		result := SpamMasker(s, substring)
+	for i, testcase := range testCases {
 
-		if result != expected {
-			t.Errorf("incorrect result, expect %s, got %s", expected, result)
-		}
-	})
-
-	t.Run("third", func(t *testing.T) {
-		s := "Here's my spammy page: hTTp://youth-elixir.com"
-		expected := "Here's my spammy page: hTTp://youth-elixir.com"
-		substring := "http://"
-		result := SpamMasker(s, substring)
-
-		if result != expected {
-			t.Errorf("incorrect result, expect %s, got %s", expected, result)
-		}
-	})
+		t.Run(testcase, func(t *testing.T) {
+			result := SpamMasker(testcase, substring)
+			if result != expectedCases[i] {
+				t.Errorf("incorrect result, expect %s, got %s", expectedCases[i], result)
+			}
+		})
+	}
 }
