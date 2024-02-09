6b6b7848a9d619d35b4468372f7e47a1a2d1a2ce
Andy <andykelevra92@gmail.com>
2024-02-10 00:04:07 +0300

    new test struct

--- main_test.go
+++ main_test.go
@@ -2,29 +2,48 @@ package main
 
 import "testing"
 
+type Case struct {
+	name           string
+	testString     string
+	expectedString string
+}
+
 func TestSpamMasker(t *testing.T) {
 
-	testCases := []string{"Here's my spammy page: http://hehefouls.netHAHAHA see you.",
-		"Here's my spammy page: http://hehefouls.net HAHAHA see you.",
-		"Here's my spammy page: hTTp://youth-elixir.com",
-		"Here's my spammy page: http://jjjjj",
-		"http://elixir.com Here's my spammy page",
-	}
-	expectedCases := []string{"Here's my spammy page: http://******************* see you.",
-		"Here's my spammy page: http://************* HAHAHA see you.",
-		"Here's my spammy page: hTTp://youth-elixir.com",
-		"Here's my spammy page: http://*****",
-		"http://********** Here's my spammy page",
+	testCases := []Case{
+		{"test1",
+			"Here's my spammy page: http://hehefouls.netHAHAHA see you.",
+			"Here's my spammy page: http://******************* see you.",
+		},
+		{"test2",
+			"Here's my spammy page: http://hehefouls.net HAHAHA see you.",
+			"Here's my spammy page: http://************* HAHAHA see you.",
+		},
+		{"test3",
+			"Here's my spammy page: hTTp://youth-elixir.com",
+			"Here's my spammy page: hTTp://youth-elixir.com",
+		},
+		{"test4",
+			"Here's my spammy page: http://jjjjj",
+			"Here's my spammy page: http://*****",
+		},
+		{"test5",
+			"http://elixir.com Here's my spammy page",
+			"http://********** Here's my spammy page",
+		},
+		{"test6",
+			"Here's my spammy page: http://",
+			"Here's my spammy page: http://",
+		},
 	}
 
 	substring := "http://"
 
-	for i, testcase := range testCases {
-
-		t.Run(testcase, func(t *testing.T) {
-			result := SpamMasker(testcase, substring)
-			if result != expectedCases[i] {
-				t.Errorf("incorrect result, expect %s, got %s", expectedCases[i], result)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := SpamMasker(tc.testString, substring)
+			if result != tc.expectedString {
+				t.Errorf("%s incorrect result, expect %s, got %s", tc.name, tc.expectedString, result)
 			}
 		})
 	}
